22725775283c5b0f9fff8ccfa05462bd6958e62a
Rishi-k-s <rishikrishna.sr@gmail.com>
2024-04-15 20:45:02 +0530

    added gui and set up test files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gdown
 import wx
 
 
+
 def initFrame():
     app = wx.App()
     frame = wx.Frame(parent=None, title='Header 001')
---
+++ test.py
@@ -0,0 +1,12 @@
+from tkinter import *
+# create root window
+root = Tk()
+ 
+# root window title and dimension
+root.title("Drive Downloader")
+# Set geometry (widthxheight)
+root.geometry('350x200')
+ 
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
